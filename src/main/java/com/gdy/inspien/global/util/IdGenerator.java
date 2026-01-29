3d56001041198a7f994c0b31c07ea53d712a06da
dongyeop00 <ehd_duq94@naver.com>
2026-01-29 09:20:24 +0900

    feat: 서버 시작 시 ORDER_ID 설정 구현

--- src/main/java/com/gdy/inspien/global/util/IdGenerator.java
+++ src/main/java/com/gdy/inspien/global/util/IdGenerator.java
@@ -9,14 +9,35 @@ public class IdGenerator {
 
     private final AtomicInteger counter = new AtomicInteger(0);
 
-    // A001, A999, B001 ... Z999 까지만 가능 26 X 1000 = 26,000개까지만 가능
+    // A000, A999, B001 ... Z999 까지만 가능 26 X 1000 = 26,000개까지만 가능
     public String generate(){
         int current = counter.getAndIncrement();
 
-        char prefix = (char) ('A' + (current + 1000) % 26);
+        char prefix = (char) ('A' + (current / 1000) % 26);
         int suffix = current % 1000;
 
         return String.format("%c%03d", prefix, suffix);
     }
 
+    public void initCounterFromMaxId(String maxId) {
+        if (maxId == null || maxId.length() < 4) {
+            counter.set(0);
+            return;
+        }
+
+        try {
+            char prefix = maxId.charAt(0);
+            int suffix = Integer.parseInt(maxId.substring(1));
+            
+            // (알파벳 순서 * 1000) + 숫자 + 1 (다음 번호)
+            int nextValue = ((prefix - 'A') * 1000) + suffix + 1;
+            counter.set(nextValue);
+        } catch (Exception e) {
+            counter.set(0);
+        }
+    }
+
+    public void reset(){
+        counter.set(0);
+    }
 }
